011af14f7e907b10769dd14f9a36016c79a8281c
Grendel <jcg@grendel.world>
2022-08-07 14:12:52 -0500

    Add user keymaps

---
+++ .config/nvim/lua/user/keymaps.lua
@@ -0,0 +1,57 @@
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+-- Remap leader key
+keymap("", "\\", "<Nop>", opts)
+vim.g.mapleader = "\\"
+vim.g.maplocalleader = "\\"
+
+
+-- Normal --
+-- Better window navigation
+keymap("n", "<M-h>", "<C-w>h", opts)
+--keymap("n", "<M-j>", "<C-w>j", opts)
+--keymap("n", "<M-k>", "<C-w>k", opts)
+keymap("n", "<M-l>", "<C-w>l", opts)
+
+keymap("n", "<leader>e", ":Lex 32<CR>", opts)
+keymap("n", "<leader>s", ":set hls!<CR>", opts)
+
+-- Window resizing with CTRL-Arrowkey
+keymap("n", "<C-Up>", "2<C-w>-", opts)
+keymap("n", "<C-Down>", "2<C-w>+", opts)
+keymap("n", "<C-Left>", "2<C-w><", opts)
+keymap("n", "<C-Right>", "2<C-w>>", opts)
+
+-- Navigate buffers
+keymap("n", "<C-n>", ":bnext<CR>", opts)
+keymap("n", "<C-p>", ":bprevious<CR>", opts)
+
+
+-- Insert --
+-- Move text up/down while in insert mode
+keymap("i", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
+keymap("i", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+
+
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Move text up and down
+keymap("v", "<A-j>", ":m '>+1<CR>gv=gv", opts)
+keymap("v", "<A-k>", ":m '<-2<CR>gv=gv", opts)
+
+-- Overwrite paste
+keymap("v", "p", '"_dp', opts)
+
+
+-- Terminal --
+-- Window switch from terminal
+keymap("t", "<A-j>", "<C-\\><C-n><C-w>j", opts)
+keymap("t", "<A-k>", "<C-\\><C-n><C-w>k", opts)
