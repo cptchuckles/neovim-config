0dce6f2b999a90b3a1d45a6dff1af8e643c441da
Grendel <jcg@grendel.world>
2022-08-21 17:15:56 -0500

    Add Gitsigns signs

--- lua/user/gitsigns.lua
+++ lua/user/gitsigns.lua
@@ -14,6 +14,14 @@ gitsigns.setup {
 	current_line_blame_formatter = "  <author>, <author_time> - (<abbrev_sha>) <summary>",
 	preview_config = { style = 'minimal', border = 'rounded' },
 
+	signs = {
+		add          = {hl = 'GitSignsAdd'   , text = '│', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'    },
+		change       = {hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn' },
+		delete       = {hl = 'GitSignsDelete', text = '▁', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn' },
+		topdelete    = {hl = 'GitSignsDelete', text = '▔', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn' },
+		changedelete = {hl = 'GitSignsChange', text = '▭', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn' },
+	},
+
 	on_attach = function(bufnr)
 		local gs = package.loaded.gitsigns
 
