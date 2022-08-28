471fb3e0a87ecf590bdfe45776ba598e0767e040
Grendel <jcg@grendel.world>
2022-08-27 19:04:42 -0500

    Use vim.api to create augroup and autocmd

--- lua/user/symbols-outline.lua
+++ lua/user/symbols-outline.lua
@@ -7,7 +7,7 @@ end
 outliner.setup {
 	highlight_hovered_item = true,
 	show_guides = true,
-	auto_preview = false,  -- Doesn't jump buffer to location, opens popup window.
+	auto_preview = false, -- Doesn't jump buffer to location, opens popup window.
 	position = 'right',
 	relative_width = true,
 	width = 25,
@@ -19,8 +19,8 @@ outliner.setup {
 	autofold_depth = nil,
 	auto_unfold_hover = true,
 	fold_markers = { '', '' },
-	keymaps = {  -- These keymaps can be a string or a table for multiple keys
-		close          = {"<Esc>", "q"},
+	keymaps = {
+		close          = { "<Esc>", "q" },
 		goto_location  = "<CR>",
 		focus_location = "<Tab>",
 		hover_symbol   = "<Space>",
@@ -36,38 +36,39 @@ outliner.setup {
 	lsp_blacklist = {},
 	symbol_blacklist = {},
 	symbols = {
-		Array         = {icon = "", hl = "TSConstant"},
-		Boolean       = {icon = "", hl = "TSBoolean"},
-		Class         = {icon = "", hl = "TSType"},
-		Constant      = {icon = "", hl = "TSConstant"},
-		Constructor   = {icon = "", hl = "TSConstructor"},
-		Enum          = {icon = "", hl = "TSType"},
-		EnumMember    = {icon = "", hl = "TSField"},
-		Event         = {icon = "", hl = "TSType"},
-		Field         = {icon = "", hl = "TSField"},
-		File          = {icon = "", hl = "TSURI"},
-		Function      = {icon = "", hl = "TSFunction"},
-		Interface     = {icon = "", hl = "TSType"},
-		Key           = {icon = "", hl = "TSType"},
-		Method        = {icon = "", hl = "TSMethod"},
-		Module        = {icon = "", hl = "TSNamespace"},
-		Namespace     = {icon = "", hl = "TSNamespace"},
-		Null          = {icon = "NULL", hl = "TSType"},
-		Number        = {icon = "", hl = "TSNumber"},
-		Object        = {icon = "", hl = "TSType"},
-		Operator      = {icon = "", hl = "TSOperator"},
-		Package       = {icon = "", hl = "TSNamespace"},
-		Property      = {icon = "", hl = "TSProperty"},
-		String        = {icon = "", hl = "TSString"},
-		Struct        = {icon = "", hl = "TSType"},
-		TypeParameter = {icon = "𝙏", hl = "TSParameter"},
-		Variable      = {icon = "", hl = "TSConstant"},
+		Array         = { icon = "", hl = "TSConstant"    },
+		Boolean       = { icon = "", hl = "TSBoolean"     },
+		Class         = { icon = "", hl = "TSType"        },
+		Constant      = { icon = "", hl = "TSConstant"    },
+		Constructor   = { icon = "", hl = "TSConstructor" },
+		Enum          = { icon = "", hl = "TSType"        },
+		EnumMember    = { icon = "", hl = "TSField"       },
+		Event         = { icon = "", hl = "TSType"        },
+		Field         = { icon = "", hl = "TSField"       },
+		File          = { icon = "", hl = "TSURI"         },
+		Function      = { icon = "", hl = "TSFunction"    },
+		Interface     = { icon = "", hl = "TSType"        },
+		Key           = { icon = "", hl = "TSType"        },
+		Method        = { icon = "", hl = "TSMethod"      },
+		Module        = { icon = "", hl = "TSNamespace"   },
+		Namespace     = { icon = "", hl = "TSNamespace"   },
+		Null          = { icon = "NULL", hl = "TSType"     },
+		Number        = { icon = "", hl = "TSNumber"      },
+		Object        = { icon = "", hl = "TSType"        },
+		Operator      = { icon = "", hl = "TSOperator"    },
+		Package       = { icon = "", hl = "TSNamespace"   },
+		Property      = { icon = "", hl = "TSProperty"    },
+		String        = { icon = "", hl = "TSString"      },
+		Struct        = { icon = "", hl = "TSType"        },
+		TypeParameter = { icon = "𝙏", hl = "TSParameter"   },
+		Variable      = { icon = "", hl = "TSConstant"    },
 	},
 }
 
-vim.cmd [[
-	augroup SymbolsOutlinerSettings
-		au!
-		au FileType Outline setl signcolumn=no
-	augroup end
-]]
+vim.api.nvim_create_autocmd('FileType', {
+	group = vim.api.nvim_create_augroup("SymbolsOutlinerSettings", { clear = true }),
+	pattern = 'Outline',
+	callback = function(opts)
+		vim.wo[vim.fn.bufwinid(opts.buf)].signcolumn = 'no'
+	end,
+})
