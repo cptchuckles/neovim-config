88d6804e18d6fe877b83c56f4e2d5ab16266fc06
Grendel <jcg@grendel.world>
2022-09-18 16:31:22 -0500

    Remove space highlights

--- lua/user/indent-blankline.lua
+++ lua/user/indent-blankline.lua
@@ -6,10 +6,6 @@ end
 
 blankline.setup {
 	char = '│',
-	space_char_highlight_list = {
-		'IndentBlanklineIndent1',
-		'IndentBlanklineIndent2',
-	},
 	show_trailing_blankline_indent = false,
 	show_cursor_context_start = true,
 	space_char_blankline = ' ',
