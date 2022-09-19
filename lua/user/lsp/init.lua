451bc5d64e4a018bbf3841f7356b9f12ca3644f4
Grendel <jcg@grendel.world>
2022-09-19 15:41:11 -0500

    Add gdscript LSP support

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -1,9 +1,16 @@
-local status_ok, _ = pcall(require, 'lspconfig')
+local status_ok, lspconfig = pcall(require, 'lspconfig')
 if not status_ok then
 	print("Couldn't load 'lspconfig'")
 	return
 end
 
-require('user.lsp.handlers').setup()
+local handlers = require('user.lsp.handlers')
+handlers.setup()
+
+lspconfig.gdscript.setup {
+	on_attach    = handlers.on_attach,
+	capabilities = handlers.capabilities,
+}
+
 require 'user.lsp.mason'
 require 'user.lsp.null-ls'
