3edc55f2e3b4b79a6db14ccc7c411a2200749d25
Grendel <jcg@grendel.world>
2022-09-29 15:59:29 -0500

    Refactor plugins into table
    This makes it possible to quickly browse categories in symbols-outliner

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -40,149 +40,163 @@ packer.init {
 	}
 }
 
--- Install plugins here
-return packer.startup(function(use)
-	-- Bare necessities
-	use 'wbthomason/packer.nvim'                              -- Let packer manage itself
-	use 'nvim-lua/popup.nvim'                                 -- Nvim implementation of Vim Popup API
-	use 'nvim-lua/plenary.nvim'                               -- Useful lua functions used by lots of plugins
+local plugins = {
+	completion = {
+		{ 'hrsh7th/nvim-cmp',                                 -- The completion engine plugin
+			commit = nil },
+		{ 'hrsh7th/cmp-buffer',                               -- Buffer completion
+			commit = nil },
+		{ 'hrsh7th/cmp-path',                                 -- Path completion
+			commit = nil },
+		{ 'hrsh7th/cmp-cmdline',                              -- Command line completion
+			commit = nil },
+		{ 'L3MON4D3/LuaSnip',                                 -- Snippet engine
+			commit = nil },
+		{ 'saadparwaiz1/cmp_luasnip',                         -- Snippet completion
+			commit = nil },
+		{ 'rafamadriz/friendly-snippets',                     -- A bunch of snippets
+			commit = nil },
+		{ 'hrsh7th/cmp-nvim-lua',                             -- Lua support for nvim-cmp
+			commit = nil },
+	},
 
-	use 'lewis6991/impatient.nvim'                            -- cache bytecode plugins for fast startup
-	use 'tjdevries/lazy.nvim'                                 -- Plugin lazy load/require
+	lsp = {
+		{ 'neovim/nvim-lspconfig',                            -- Official community LSP configs
+			commit = nil },
+		{ 'hrsh7th/cmp-nvim-lsp',                             -- LSP support for nvim-cmp
+			commit = nil },
+		{ 'Issafalcon/lsp-overloads.nvim',                    -- signature overload cycler
+			commit = nil },
+		{ 'williamboman/mason.nvim',                          -- LSP/DAP/Format/Lint manager
+			commit = nil },
+		{ 'williamboman/mason-lspconfig.nvim',                -- Setup lspconfig for mason-installed LSPs
+			commit = nil },
+		{ 'Hoffs/omnisharp-extended-lsp.nvim',                -- Omnisharp specific bullshit
+			commit = nil },
+		{ 'jose-elias-alvarez/null-ls.nvim',                  -- Null LS
+			commit = nil },
+		--  { 'ThePrimeagen/refactoring.nvim',                   -- Refactoring
+		-- 	commit = nil },
+		{ 'j-hui/fidget.nvim',                                -- LSP Progress fidget spinner
+			commit = nil },
+	},
 
-	-- Completion
-	use { 'hrsh7th/nvim-cmp',                                    -- The completion engine plugin
-		commit = nil }
-	use { 'hrsh7th/cmp-buffer',                                  -- Buffer completion
-		commit = nil }
-	use { 'hrsh7th/cmp-path',                                    -- Path completion
-		commit = nil }
-	use { 'hrsh7th/cmp-cmdline',                                 -- Command line completion
-		commit = nil }
-	use { 'L3MON4D3/LuaSnip',                                    -- Snippet engine
-		commit = nil }
-	use { 'saadparwaiz1/cmp_luasnip',                            -- Snippet completion
-		commit = nil }
-	use { 'rafamadriz/friendly-snippets',                        -- A bunch of snippets
-		commit = nil }
-	use { 'hrsh7th/cmp-nvim-lua',                                -- Lua support for nvim-cmp
-		commit = nil }
+	dap = {
+		{ 'mfussenegger/nvim-dap',                            -- DAP
+			commit = nil },
+		{ 'rcarriga/nvim-dap-ui',                             -- DAP UI
+			commit = nil },
+		{ 'theHamsta/nvim-dap-virtual-text',                  -- DAP Virtual Text
+			commit = nil },
+		{ 'nvim-telescope/telescope-dap.nvim',
+			commit = nil },
+	},
 
-	-- LSP
-	use { 'neovim/nvim-lspconfig',                               -- Official community LSP configs
-		commit = nil }
-	use { 'hrsh7th/cmp-nvim-lsp',                                -- LSP support for nvim-cmp
-		commit = nil }
-	use { 'Issafalcon/lsp-overloads.nvim',                       -- signature overload cycler
-		commit = nil }
-	use { 'williamboman/mason.nvim',                             -- LSP/DAP/Format/Lint manager
-		commit = nil }
-	use { 'williamboman/mason-lspconfig.nvim',
-		commit = nil }
-	use { 'Hoffs/omnisharp-extended-lsp.nvim',                   -- Omnisharp specific bullshit
-		commit = nil }
-	use { 'jose-elias-alvarez/null-ls.nvim',                     -- Null LS
-		commit = nil }
-	-- use { 'ThePrimeagen/refactoring.nvim',                       -- Refactoring
-	-- 	commit = nil }
-	use { 'j-hui/fidget.nvim',                                   -- LSP Progress fidget spinner
-		commit = nil }
+	quality_of_life = {
+		themeing = {
+			{ 'jlcrochet/vim-razor',                          -- Attempt to edit razor with neovim
+				commit = nil },
+			{ 'LunarVim/colorschemes',                        -- a bunch of colorschemes
+				commit = nil },
+			{ 'folke/tokyonight.nvim',                        -- TokyoNight theme
+				commit = nil },
+			{ 'kyazdani42/nvim-web-devicons',                 -- more icons and shit
+				commit = nil },
+			{ 'Darazaki/indent-o-matic',                      -- Auto-detect buffer indentation
+				commit = nil },
+			{ 'norcalli/nvim-colorizer.lua',                  -- Colorize hex color codes
+				commit = nil },
+		},
+		editing = {
+			{ 'numToStr/Comment.nvim',                        -- Auto-commenting
+				commit = nil },
+			{ 'RRethy/vim-illuminate',                        -- Cursor-word highlighter + text objects
+				commit = nil },
+			{ 'andymass/vim-matchup',                         -- Better % operator (keywords and shit)
+				commit = 'e59d5c73f1bf696c7de9fc13090a240d1ceb13f7',
+				event = 'VimEnter',
+				config = function()
+					vim.g.matchup_matchparen_deferred = 1
+					vim.g.matchup_matchparen_offscreen = {}
+				end,
+			},
+			{ 'iamcco/markdown-preview.nvim',                 -- Markdown previewer in web browser
+				commit = nil,
+				run = function() vim.fn['mkdp#util#install']() end,
+				cmd = 'MarkdownPreview',
+				setup = function() vim.g.mkdp_filetypes = {'markdown'} end,
+				ft = 'markdown',
+				opt = true,
+			},
+		},
+		layout = {
+			{ 'lukas-reineke/indent-blankline.nvim',          -- Indentation fanciness
+				commit = nil },
+			{ 'lewis6991/gitsigns.nvim',                      -- Git Signs
+				commit = nil },
+			{ 'simrat39/symbols-outline.nvim',                -- Symbols outliner
+				commit = nil },
+			{ 'akinsho/bufferline.nvim',                      -- Bufferline - Tabs, but buffers!
+				commit = nil },
+			{ 'moll/vim-bbye',                                -- Buffer deletion done right
+				commit = nil },
+			{ 'glepnir/galaxyline.nvim',                      -- Sickass statusline
+				commit = nil,
+				branch = 'main',
+				requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+			},
+		},
+	},
 
-	-- DAP
-	use { 'mfussenegger/nvim-dap',                               -- DAP
-		commit = nil }
-	use { 'rcarriga/nvim-dap-ui',                                -- DAP UI
-		commit = nil }
-	use { 'theHamsta/nvim-dap-virtual-text',                     -- DAP Virtual Text
-		commit = nil }
-	use { 'nvim-telescope/telescope-dap.nvim',
-		commit = nil }
-
-	-- Quality of Life enhancements
-	use { 'jlcrochet/vim-razor',                                 -- Attempt to edit razor with neovim
-		commit = nil }
-	use { 'LunarVim/colorschemes',                               -- a bunch of colorschemes
-		commit = nil }
-	use { 'folke/tokyonight.nvim',                               -- TokyoNight theme
-		commit = nil }
-	use { 'kyazdani42/nvim-web-devicons',                        -- more icons and shit
-		commit = nil }
-	use { 'lewis6991/gitsigns.nvim',                             -- Git Signs
-		commit = nil }
-	use { 'lukas-reineke/indent-blankline.nvim',                 -- Indentation fanciness
-		commit = nil }
-	use { 'numToStr/Comment.nvim',                               -- Auto-commenting
-		commit = nil }
-	use { 'nvim-telescope/telescope.nvim',                       -- Telescope
-		commit = nil }
-	use { 'nvim-telescope/telescope-ui-select.nvim',             -- Use telescope for more UI things
-		commit = nil }
-	use { 'folke/trouble.nvim',                                  -- Pretty qickfix list
-		commit = nil }
-	use { 'simrat39/symbols-outline.nvim',                       -- Symbols outliner
-		commit = nil }
-	use { 'RRethy/vim-illuminate',                               -- Cursor-word highlighter + text objects
-		commit = nil }
-	use { 'akinsho/bufferline.nvim',                             -- Bufferline - Tabs, but buffers!
-		commit = nil }
-	use { 'Darazaki/indent-o-matic',                             -- Auto-detect buffer indentation
-		commit = nil }
-	use { 'moll/vim-bbye',                                       -- Buffer deletion done right
-		commit = nil }
-	use { 'norcalli/nvim-colorizer.lua',                         -- Colorize hex color codes
-		commit = nil }
-	use {
-		'kyazdani42/nvim-tree.lua',                           -- NvimTree
-		commit = nil,
-		requires = { 'kyazdani42/nvim-web-devicons' }
-	}
-
-	use {                                                     -- TREESITTER
-		{
-			'nvim-treesitter/nvim-treesitter',
+	treesitter = {
+		{ 'nvim-treesitter/nvim-treesitter',
 			commit = '27cba73df9ddca00c066c7ebb514576a99bb8b2c',
 			run = function()
 				require('nvim-treesitter.install').update({ with_sync = true })
 			end,
 		},
-		{ 'nvim-treesitter/playground',                         -- TSPlayground to see the AST of a file
+		{ 'nvim-treesitter/playground',                       -- TSPlayground to see the AST of a file
 			commit = nil },
-		{ 'nvim-treesitter/nvim-treesitter-context',            -- Sticky code context
+		{ 'nvim-treesitter/nvim-treesitter-context',          -- Sticky code context
 			commit = nil },
-		{ 'p00f/nvim-ts-rainbow',                               -- Color-code brackets and parens and shit
+		{ 'p00f/nvim-ts-rainbow',                             -- Color-code brackets and parens and shit
 			commit = nil },
-	}
+	},
 
-	use {
-		'andymass/vim-matchup',                               -- Better % operator (keywords and shit)
-		commit = 'e59d5c73f1bf696c7de9fc13090a240d1ceb13f7',
-		event = 'VimEnter',
-		config = function()
-			vim.g.matchup_matchparen_deferred = 1
-			vim.g.matchup_matchparen_offscreen = {}
-		end,
-	}
+	workspace_tools = {
+		{ 'kdheepak/lazygit.nvim',                            -- LazyGit
+			commit = nil },
+		{ 'nvim-telescope/telescope.nvim',                    -- Telescope
+			commit = nil },
+		{ 'nvim-telescope/telescope-ui-select.nvim',          -- Use telescope for more UI things
+			commit = nil },
+		{ 'folke/trouble.nvim',                               -- Pretty qickfix list
+			commit = nil },
+		{ 'kyazdani42/nvim-tree.lua',                         -- NvimTree
+			commit = nil,
+			requires = { 'kyazdani42/nvim-web-devicons' },
+		},
+	},
+}
 
-	-- Useful tools
-	use { 'kdheepak/lazygit.nvim',                               -- LazyGit
-		commit = nil }
-	use {
-		'iamcco/markdown-preview.nvim',                       -- Markdown previewer in web browser
-		commit = nil,
-		run = function() vim.fn['mkdp#util#install']() end,
-		cmd = 'MarkdownPreview',
-		setup = function() vim.g.mkdp_filetypes = {'markdown'} end,
-		ft = 'markdown',
-		opt = true,
-	}
+-- Install plugins here
+return packer.startup(function(use)
+	-- Bare necessities
+	use 'wbthomason/packer.nvim'    -- Let packer manage itself
+	use 'nvim-lua/popup.nvim'       -- Nvim implementation of Vim Popup API
+	use 'nvim-lua/plenary.nvim'     -- Useful lua functions used by lots of plugins
 
-	-- Sickass statusline thing
-	use {
-		'glepnir/galaxyline.nvim',
-		commit = nil,
-		branch = 'main',
-		requires = { 'kyazdani42/nvim-web-devicons', opt = true },
-	}
+	use 'lewis6991/impatient.nvim'  -- cache bytecode plugins for fast startup
+	use 'tjdevries/lazy.nvim'       -- Plugin lazy load/require
+
+	use(plugins.completion)
+	use(plugins.lsp)
+	use(plugins.dap)
+	use(plugins.quality_of_life.themeing)
+	use(plugins.quality_of_life.editing)
+	use(plugins.quality_of_life.layout)
+	use(plugins.treesitter)
+	use(plugins.workspace_tools)
 
 	-- Automatically set configuration after cloning packer.nvim
 	-- Keep this at the end
