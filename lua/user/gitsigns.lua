b1262de775577b5f035ffad4233598afa13f261e
Grendel <jcg@grendel.world>
2022-12-02 13:19:31 -0600

    Add untracked gitsign

--- lua/user/gitsigns.lua
+++ lua/user/gitsigns.lua
@@ -15,11 +15,12 @@ gitsigns.setup {
 	preview_config = { style = 'minimal', border = 'rounded' },
 
 	signs = {
-		add          = {hl = 'GitSignsAdd'   , text = '┃', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'    },
-		change       = {hl = 'GitSignsChange', text = '┃', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn' },
-		delete       = {hl = 'GitSignsDelete', text = '▁', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn' },
-		topdelete    = {hl = 'GitSignsDelete', text = '▔', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn' },
-		changedelete = {hl = 'GitSignsChange', text = '┋', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn' },
+		add          = { hl = 'GitSignsAdd'   , text = '┃', numhl = 'GitSignsAddNr'   , linehl = 'GitSignsAddLn'    },
+		change       = { hl = 'GitSignsChange', text = '┃', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
+		delete       = { hl = 'GitSignsDelete', text = '▁', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
+		topdelete    = { hl = 'GitSignsDelete', text = '▔', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
+		changedelete = { hl = 'GitSignsChange', text = '┋', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
+		untracked    = { hl = 'GitSignsAdd'   , text = '', numhl = 'GitSignsAddNr'   , linehl = 'GitSignsAddLn'    },
 	},
 
 	on_attach = require('user.settings.keymaps').gitsigns
