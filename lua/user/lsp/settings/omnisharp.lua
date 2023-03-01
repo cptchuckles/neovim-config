084fe6599d06b26f546a2709a44cac8a499c600e
Grendel <jcg@grendel.world>
2023-03-01 14:50:21 -0600

    Add Microsoft.AspNetCore.Razor.OmniSharpPlugin.dll to Omnisharp
    This requires a build of https://github.com/dotnet/razor to be
    published to $HOME/.razor/
    
    This does not yet achieve feature-parity with VSCode's razor
    support.  This allows razor component references to appear in C#
    files: for example, given the following line in a Program.cs,
    ```cs
    builder.RootComponents.Add<App>("#app");
    ```
    where `App` is defined in `App.razor`, will no longer show an error
    diagnostic, as the Razor plug-in now compiles
    .razor__bg__virtual.cs virtual source files for all .razor files in
    a project.  textDoucment/reference event handlers of C# classes
    inside of .razor files will also partially work, although further
    configuration of neovim is required in order to elide the
    generated, virtual source files from the references so that neovim
    won't throw an error attempting to open non-existing files.
    
    Despite my hopes, this does not supply C# language support for
    @code blocks inside of .razor component files.  I suspect that
    omnisharp needs to work in tandem with rzls somehow in order to
    achieve feature-parity with VSCode.

--- lua/user/lsp/settings/omnisharp.lua
+++ lua/user/lsp/settings/omnisharp.lua
@@ -1,49 +1,23 @@
-return {
-	organize_imports_on_format = true,
-	enable_import_completion = true,
-	enable_roslyn_analyzers = false,  -- Consider keeping this off so it stops talking shit about my code
-	filetypes = { "cs", "vb", --[[ "cshtml", "razor" ]] },  -- Absolutely futile.
+local O = {}
 
-	handlers = {
-		["textDocument/definition"] = require('omnisharp_extended').handler,
-	},
+O.organize_imports_on_format = true
+O.enable_import_completion = true
+O.enable_roslyn_analyzers = true
+O.analyze_open_documents_only = true
 
-	-- These are goddamn useless
-	-- They get set and you can see them in the config, but they don't appear to *do* anything
-	-- because I assume they control launch options *from VSCode*, but nvim-lspconfig doesn't
-	-- seem to give half a rat's ass about these settings.
-	-- I found the above snake_case equivalents to some of these (e.g. enable_import_completion)
-	-- by opening a C# source file and typing `:lua =vim.lsp.get_active_clients()[1]` and
-	-- reading through the table that gets dumped out.
-	settings = {
-		csharp = {
-			inlayHints = {
-				parameters = {
-					enabled = true,
-					forIndexerParameters = true,
-					forLiteralParameters = true,
-					forObjectCreationParameters = true,
-					forOtherParameters = true,
-				},
-				types = {
-					enabled = true,
-					forImplicitObjectCreation = true,
-					forImplicitVariableTypes = true,
-					forLambdaParameterTypes = true,
-				},
-			},
-		},
-		omnisharp = {
-			enableImportCompletion = true,
-			enableMsBuildLoadProjectsOnDemant = false,
-			enableRoslynAnalyzers = true,
-			organizeImportsOnFormat = true,
-		},
-		razor = {
-			plugin = {
-				-- Nope
-				path = "/home/grendel/.vscode/extensions/ms-dotnettools.csharp-1.25.0-linux-x64/.razor/rzls.dll",
-			},
-		},
-	},
+O.handlers = {
+	["textDocument/definition"] = require('omnisharp_extended').handler,
 }
+
+O.cmd = { "omnisharp" }
+O.filetypes = { 'cs', 'vb', 'razor', 'cshtml' }
+
+local razor_dir = vim.fn.getenv("HOME") .. "/.razor"
+if vim.fn.isdirectory(razor_dir) then
+	vim.list_extend(O.cmd, {
+		"--plugin",
+		razor_dir .. "/OmniSharpPlugin/Microsoft.AspNetCore.Razor.OmniSharpPlugin.dll"
+	})
+end
+
+return O
