6b60b113dea3158292006280e03f5f4248d5d76b
Grendel <jcg@grendel.world>
2023-04-11 17:51:22 -0500

    nvim-tree: don't update pwd for 'term' and '$metadata$' buffers

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -17,7 +17,7 @@ tree.setup {
 	update_focused_file = {
 		enable = true,
 		update_root = true,
-		ignore_list = { 'help' },
+		ignore_list = { 'help', 'term', '$metadata$' },
 	},
 	diagnostics = {
 		enable = true,
