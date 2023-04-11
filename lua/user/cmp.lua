acb150f62f24d0a0d6ad40a05ea708308b3259c0
Grendel <jcg@grendel.world>
2023-04-11 17:50:04 -0500

    Erase dead code

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -15,11 +15,6 @@ cmp.setup {
 		end,
 	},
 
-	-- window = {
-	-- 	completion = cmp.config.window.bordered(),
-	-- 	documentation = cmp.config.window.bordered(),
-	-- },
-
 	mapping = cmp.mapping.preset.insert({
 		['<C-b>'] = cmp.mapping.scroll_docs(-4),
 		['<C-f>'] = cmp.mapping.scroll_docs(4),
