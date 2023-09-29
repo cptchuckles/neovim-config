bad2da1ab956a9164e4a969450a246223560f88c
Grendel <jcg@grendel.world>
2023-09-29 15:22:39 -0500

    lsp: Move diagnostics and theme setups out of init

---
+++ lua/user/lsp/diagnostics.lua
@@ -0,0 +1,42 @@
+local signs = {
+	{ name = "DiagnosticSignError", text = "" },
+	{ name = "DiagnosticSignWarn",  text = "" },
+	{ name = "DiagnosticSignInfo",  text = "" },
+	{ name = "DiagnosticSignHint",  text = "ﯧ" },
+}
+
+for _, sign in ipairs(signs) do
+	vim.fn.sign_define(sign.name, {
+		texthl = sign.name,
+		text   = sign.text,
+		numhl  = "",
+	})
+end
+
+vim.diagnostic.config {
+	virtual_text = {
+		severity = {
+			min = vim.diagnostic.severity.WARN,
+		},
+		spacing  = 2,
+		prefix   = "⋮",
+	},
+	signs = {
+		active = signs,
+	},
+	update_in_insert = true,
+	severity_sort    = true,
+	underline = {
+		severity = {
+			min = vim.diagnostic.severity.INFO,
+		},
+	},
+	float = {
+		focusable = false,
+		style     = "minimal",
+		border    = "rounded",
+		source    = "if_many",
+		header    = "",
+		prefix    = "· ",
+	},
+}
--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -1,57 +1,5 @@
-local signs = {
-	{ name = "DiagnosticSignError", text = "" },
-	{ name = "DiagnosticSignWarn",  text = "" },
-	{ name = "DiagnosticSignInfo",  text = "" },
-	{ name = "DiagnosticSignHint",  text = "ﯧ" },
-}
-
-for _, sign in ipairs(signs) do
-	vim.fn.sign_define(sign.name, {
-		texthl = sign.name,
-		text   = sign.text,
-		numhl  = "",
-	})
-end
-
-local config = {
-	virtual_text = {
-		severity = {
-			min = vim.diagnostic.severity.WARN,
-		},
-		spacing  = 2,
-		prefix   = "⋮",
-	},
-	signs = {
-		active = signs,
-	},
-	update_in_insert = true,
-	severity_sort    = true,
-	underline = {
-		severity = {
-			min = vim.diagnostic.severity.INFO,
-		},
-	},
-	float = {
-		focusable = false,
-		style     = "minimal",
-		border    = "rounded",
-		source    = "if_many",
-		header    = "",
-		prefix    = "· ",
-	},
-}
-
-vim.diagnostic.config(config)
-
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-	border = "rounded",
-})
-
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-	border = "rounded",
-})
-
-require('lspconfig.ui.windows').default_options.border = "rounded"
+require 'user.lsp.diagnostics'
+require 'user.lsp.theme'
 
 require 'user.lsp.mason'
 require 'user.lsp.null-ls'
