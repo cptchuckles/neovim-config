301ed1a881b8eeb678bb892bf1b00a1cb6235ace
Grendel <jcg@grendel.world>
2024-01-17 16:45:26 -0600

    lsp(java): use system default jdk runtime

--- lua/user/lsp/custom/java.lua
+++ lua/user/lsp/custom/java.lua
@@ -1,6 +1,18 @@
 return function(config)
-	require('java').setup()
-	require('lspconfig').jdtls.setup(config)
+	require('java').setup({ jdk = { auto_install = false }})
+	require('lspconfig').jdtls.setup(vim.tbl_deep_extend("error", config, {
+		settings = {
+			java = {
+				configuration = {
+					runtimes = {
+						name = "system-jdk",
+						path = "/usr/lib/jvm/default-runtime",
+						default = true,
+					}
+				}
+			}
+		}
+	}))
 
 	vim.api.nvim_create_autocmd('BufWritePost', {
 		group = vim.api.nvim_create_augroup('JavaAutoCompile', { clear = true }),
