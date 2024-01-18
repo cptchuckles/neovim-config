62a99def01b4c5e8a66dd445c05f27e2ee6ca32b
Grendel <jcg@grendel.world>
2024-01-17 21:59:09 -0600

    au(java): move autocmd to ftplugin and out of lsp setup

--- lua/user/lsp/custom/java.lua
+++ lua/user/lsp/custom/java.lua
@@ -13,23 +13,4 @@ return function(config)
 			}
 		}
 	}))
-
-	vim.api.nvim_create_autocmd('BufWritePost', {
-		group = vim.api.nvim_create_augroup('JavaAutoCompile', { clear = true }),
-		desc = 'Build gradle or maven projects post write',
-		pattern = '*.java',
-		callback = function()
-			local compiler
-			if vim.fn.filereadable("mvnw") == 1 then
-				compiler = "!./mvnw compile"
-			elseif vim.fn.filereadable("gradlew") == 1 then
-				compiler = "!./gradlew build"
-			else
-				return
-			end
-
-			print("Building " .. vim.fn.expand('%:t:r'))
-			vim.fn.execute(compiler, "silent")
-		end,
-	})
 end
