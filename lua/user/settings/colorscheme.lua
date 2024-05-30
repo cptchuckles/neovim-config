27156962a40237c0d28d88dd7350ba6156a9ef1a
Grendel <jcg@grendel.world>
2024-05-30 12:20:27 -0500

    make highlights more flexible for light colorschemes

--- lua/user/settings/colorscheme.lua
+++ lua/user/settings/colorscheme.lua
@@ -9,15 +9,14 @@ local function make_theme_augroup()
       \|  highlight! Todo cterm=bold gui=bold ctermbg=Yellow ctermfg=Black guibg=Yellow guifg=Black
       \|  highlight! Comment cterm=italic gui=italic
       \|  highlight! link TSComment Comment
-      \|  highlight! Whitespace cterm=NONE ctermfg=8 guifg=#3a3a3a
+      \|  highlight! link Whitespace Comment
+      \|  highlight! Whitespace cterm=NONE ctermbg=NONE gui=NONE guibg=NONE
       \|  highlight! link WinSeparator LineNr
       \|  highlight! GitSignsCurrentLineBlame cterm=italic,bold ctermfg=Lightgray gui=italic,bold guifg=#4A4A4A
       \|  highlight! DiagnosticVirtualTextError cterm=bold,italic gui=bold,italic ctermfg=darkred guifg=darkred guibg=NONE
       \|  highlight! DiagnosticVirtualTextWarn cterm=bold,italic gui=bold,italic ctermfg=yellow guifg=#777700 guibg=NONE
       \|  highlight! DiagnosticVirtualTextInfo cterm=bold,italic gui=bold,italic ctermfg=lightyellow guifg=#666644 guibg=NONE
-      \|  highlight! IndentBlanklineChar guifg=#2b2f38 gui=nocombine
       \|  highlight! link IndentBlanklineSpaceChar Whitespace
-      \|  highlight! link IndentBlanklineIndent1 CursorColumn
       \|  highlight! IndentBlanklineIndent2 guibg=NONE gui=nocombine
       \|  highlight! link IlluminatedWordText LspReferenceText
       \|  highlight! link IlluminatedWordRead LspReferenceRead
